fde0115d56ae379ab88d11fa0ebfe4501c5f1115
FaustXVI <xavier.detant@gmail.com>
2016-09-25 15:58:37 +0200

    une fonction et un type

--- Specs.hs
+++ Specs.hs
@@ -1,7 +1,14 @@
 import Test.Hspec
 
+type Card = (Int,Char)
+
+rank :: Card -> Int
+rank _ = 2
+
 main = hspec $ describe "" $ do
         it "" $ do
             sqrt (9 * 9) `shouldBe` 9
             sqrt 9 * 9 `shouldBe` 27
+        it "" $ do
+            rank (2,'C') `shouldBe` 2
 

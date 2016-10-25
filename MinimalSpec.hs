8dc1f787ad78b0ff43beaee287f8ab465934b711
ToF- <cthibaut@octo.com>
2016-10-25 07:49:38 +0200

    editing tests slide

--- MinimalSpec.hs
+++ MinimalSpec.hs
@@ -1,9 +1,9 @@
 import Test.Hspec
 
 main = hspec $ 
-    describe "a test" $ do
-        it "should pass" $ do
-            2+2  `shouldBe` 4
-        
-        it "should not fail" $ do
-            2*2 `shouldBe` 4
+    describe "a test" $ 
+        (it "should pass" $ 
+            2+2  `shouldBe` 4)
+        >>
+        (it "should not fail" $
+            2*2 `shouldBe` 4)

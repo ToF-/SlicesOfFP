b1282fa3265d1279bd42723b680bc3668e7dc50f
ToF- <cthibaut@octo.com>
2017-03-20 19:56:54 +0100

    fixed 005

--- code/005.hs
+++ code/005.hs
@@ -1,6 +1,6 @@
 -- 005.hs
 import Test.Hspec
 main = hspec
-(describe "a test"
- (it "should pass"
-  (2+2  ‘shouldBe‘ 4)))
+    (describe "a test"
+         (it "should pass"
+            (2+2  `shouldBe` 4)))

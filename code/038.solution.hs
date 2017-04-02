97e995e644f1d1d22e3cd303b213d8028cd2476b
FaustXVI <xavier.detant@gmail.com>
2017-04-02 16:07:57 +0200

    Add solutions

---
+++ code/038.solution.hs
@@ -0,0 +1,42 @@
+data Suit = Hearts | Clubs | Diamonds | Spades
+    deriving (Eq, Show)
+
+data Rank = Two | Three | Four | Five | Six | Seven | Eight 
+          | Nine | Ten | Jack | Queen | King | Ace
+    deriving (Eq, Ord, Enum, Show)
+
+type Card = (Rank, Suit)
+type Hand = [Card]
+
+rank :: Card -> Rank
+rank = fst
+
+suit :: Card -> Suit
+suit = snd
+
+
+card :: String -> Card
+card [r,s] = (charToRank r, charToSuit s)
+
+charToRank :: Char -> Rank
+charToRank 'A' = toEnum 12
+charToRank 'K' = toEnum 11
+charToRank 'Q' = toEnum 10
+charToRank 'J' = toEnum 9
+charToRank 'T' = toEnum 8
+charToRank c   = toEnum $ (Data.Char.digitToInt c) - 2
+
+charToSuit :: Char -> Suit
+charToSuit 'h' = Hearts
+charToSuit 'c' = Clubs
+charToSuit 'd' = Diamonds
+charToSuit 's' = Spades
+
+cards :: String -> Hand
+cards = (map card) . words
+
+ranks :: Hand -> [Rank]
+ranks = (Data.List.sortBy (flip compare)) . (map rank)
+
+groups :: Hand -> [[Rank]]
+groups = (Data.List.sortBy (flip (Data.Ord.comparing length))) . Data.List.group . ranks
--- code/verify.sh
+++ code/verify.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
-for file in `ls 0*.hs | grep -v solution | grep -E -v '(014|016|022|024|025|029)'`
+for file in `ls 0*.hs | grep -v solution | grep -E -v '(014|016|022|024|025|029|039|045|047|050|052)'`
 do
     if [ ! \( -L $file \) ]
     then
         echo "Executing $file"
         solutionFile=${file%.*}.solution.hs
+        if [[ "$file" =~ "032" ]]
+        then
+            solutionFile="023.solution.hs"
+        fi
         if [ -e $solutionFile ]
         then
 cat <<EOF | cat - imports.hs $file $solutionFile | runhaskell || exit 2
 import Data.Char
+import Data.List
+import Data.Ord
 EOF
         else
             runhaskell $file || exit 1
         fi
     fi
 done
+
+cat <<EOF | cat - 032.solution.hs 023.solution.hs | runhaskell || exit 2
+import Data.Char
+import Data.List
+import Data.Ord
+EOF

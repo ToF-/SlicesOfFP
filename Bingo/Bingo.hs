97d6a3a775512b1bad539aabb202d7ca066cc6d2
ToF- <cthibaut@octo.com>
2017-04-03 19:21:09 +0200

    usage: runhaskell Bingo.hs >sheets.tex 42; pdflatex sheets.tex

--- Bingo/Bingo.hs
+++ Bingo/Bingo.hs
@@ -1,12 +1,19 @@
+import System.Environment
 import System.Random
 import System.Random.Shuffle
 
 preamble = unlines $ 
-    ["\\documentclass[10pt]{article}",
-    "\\usepackage{xstring}",
+    ["\\documentclass[A4paper,10pt]{article}",
+    "\\usepackage[english]{babel}",
+    "\\usepackage[latin1]{inputenc}",
+    "\\usepackage[usenames,dvipsnames]{xcolor}",
+    "\\usepackage{listings}",
+    "\\usepackage{verbatim}",
+    "\\usepackage{fancyvrb}",
+    "\\usepackage{moreverb}",
     "\\usepackage{tikz}",
     "\\usetikzlibrary{calc}",
-    "\\newcommand{\\Size}{3.25cm}",
+    "\\newcommand{\\Size}{2.36cm}",
     "\\tikzset{Square/.style={",
     "inner sep=0pt,",
     "text width=\\Size, ",
@@ -15,11 +22,18 @@ preamble = unlines $
     "align=center,",
     "}",
     "}",
+    "\\input{../Beamer/Haskell.tex}",
+    "\\pagestyle{empty}",
     "\\begin{document}",
+    "\\lstMakeShortInline[language=Haskell,basicstyle=\\ttfamily]~"]
+postamble = unlines $ ["\\end{document}"]
+
+prepicture = unlines $ [
+    "\\begin{center}",
     "\\begin{tikzpicture}[draw=black, thick, x=\\Size,y=\\Size]"]
-postamble = unlines $
-    ["\\end{tikzpicture}",
-     "\\end{document}"]
+postpicture= unlines $ [
+     "\\end{tikzpicture}",
+     "\\end{center}"]
 
 showNode :: (Int, String) -> String
 showNode (n, s) = "\\node [Square] at ($(" ++ show c ++ ", " ++ show r ++ ")-(0.5,0.5)$) {\\large "++ s ++"};"
@@ -27,10 +41,20 @@ showNode (n, s) = "\\node [Square] at ($(" ++ show c ++ ", " ++ show r ++ ")-(0.
     c = n `mod` 5
     r = n `div` 5
 
-ws = words "foo bar qux baz baaz meh blah wow"
+ws = ["\\emph{pattern\\\\matching}","~head~","~tail~","~sort~","~sortBy~","~compare~","~type~","~data~","type class","~deriving~","~Eq~","~Ord~","~Enum~","~Show~",
+            "\\emph{guards}","\\emph{lists}","\\emph{tuples}","~comparing~","\\emph{function\\\\composition}","~map~","~flip~","~group~","~fromEnum~","~toEnum~","~concat~"]
 
-main = do 
-    gen <- getStdGen
+sheet :: StdGen -> Int -> IO ()
+sheet _ 0 = putStr ""
+sheet gen n = do 
     let items = zip [0..] $ shuffle' ws (length ws) gen
-        text = preamble ++ (concatMap showNode items) ++ postamble
+        text = prepicture ++ (concatMap showNode items) ++ postpicture
     putStr $ text
+    gen' <- newStdGen
+    sheet gen' (n-1)
+main = do
+    args <- getArgs
+    gen <- newStdGen
+    putStr preamble
+    sheet gen (read (args!!0))
+    putStr postamble
